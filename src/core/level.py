7823ceb1a3af0332135d166bd3e1855fbf7acf82
Krytoypos <ghaskag10@gmail.com>
2026-05-11 19:40:54 +0300

    feat: implement functional MVP with physics, collisions and level loading

--- src/core/level.py
+++ src/core/level.py
@@ -1,18 +1,83 @@
 import pygame
 
-from src.settings import BLACK
+from settings import BLACK, TILE_SIZE
+from sprites.player import Player
+from sprites.platform import Platform
 
 
 class Level:
-
     def __init__(self, level_path):
 
         self.level_path = level_path
 
+        # Группы спрайтов
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+        # Временная карта уровня
+        self.level_map = [
+            "########################################",
+            "#......................................#",
+            "#......................................#",
+            "#......................................#",
+            "#......................................#",
+            "#......................................#",
+            "#......................................#",
+            "#......................................#",
+            "#......................................#",
+            "#......................................#",
+            "#......................................#",
+            "#......................................#",
+            "#...................###................#",
+            "#......................................#",
+            "#..........###.........................#",
+            "#......................................#",
+            "#......................................#",
+            "#.....P................................#",
+            "########################################"
+        ]
+
+        # Создание объектов уровня
+        self.create_map()
+
+    def create_map(self):
+
+        for row_index, row in enumerate(self.level_map):
+
+            for col_index, cell in enumerate(row):
+
+                x = col_index * TILE_SIZE
+                y = row_index * TILE_SIZE
+
+                # Платформа
+                if cell == "#":
+
+                    platform = Platform((x, y))
+
+                    # Добавляем:
+                    # - в отрисовку
+                    # - в коллизии
+                    self.visible_sprites.add(platform)
+                    self.obstacle_sprites.add(platform)
+
+                # Игрок
+                elif cell == "P":
+
+                    self.player = Player((x, y))
+
+                    # Игрок только в видимые
+                    self.visible_sprites.add(self.player)
+
     def update(self, dt):
 
-        pass
+        # Обновляем все спрайты
+        # Передаем платформы для коллизий
+        self.visible_sprites.update(dt, self.obstacle_sprites)
 
     def draw(self, screen):
 
+        # Фон
         screen.fill(BLACK)
+
+        # Отрисовка всех объектов
+        self.visible_sprites.draw(screen)
--- src/sprites/player.py
+++ src/sprites/player.py
@@ -1,11 +1,17 @@
 import pygame
 
+from settings import (
+    PLAYER_SPEED,
+    GRAVITY,
+    JUMP_POWER
+)
+
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, settings):
-        super().__init__()
 
-        self.settings = settings
+    def __init__(self, pos):
+
+        super().__init__()
 
         # Временный спрайт
         self.image = pygame.Surface((32, 64))
@@ -21,25 +27,27 @@ class Player(pygame.sprite.Sprite):
             60
         )
 
-        # Float-позиция для плавного движения
+        # Float-позиция
         self.pos = pygame.Vector2(self.hitbox.topleft)
 
         # Скорость
         self.velocity = pygame.Vector2(0, 0)
 
+        # На земле?
         self.on_ground = False
 
     def get_input(self):
+
         keys = pygame.key.get_pressed()
 
         # Движение по X
         self.velocity.x = 0
 
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.velocity.x = self.settings.PLAYER_SPEED
+            self.velocity.x = PLAYER_SPEED
 
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.velocity.x = -self.settings.PLAYER_SPEED
+            self.velocity.x = -PLAYER_SPEED
 
         # Прыжок
         if (
@@ -47,26 +55,31 @@ class Player(pygame.sprite.Sprite):
             or keys[pygame.K_UP]
             or keys[pygame.K_w]
         ) and self.on_ground:
+
             self.jump()
 
     def jump(self):
-        self.velocity.y = self.settings.JUMP_FORCE
+
+        self.velocity.y = JUMP_POWER
         self.on_ground = False
 
     def apply_gravity(self, dt):
-        self.velocity.y += self.settings.GRAVITY * dt
+
+        self.velocity.y += GRAVITY * dt
 
         # Ограничение скорости падения
         if self.velocity.y > 1200:
             self.velocity.y = 1200
 
     def horizontal_movement_collision(self, platforms, dt):
+
         # Движение по X
         self.pos.x += self.velocity.x * dt
         self.hitbox.x = round(self.pos.x)
 
-        # Проверка коллизий
+        # Коллизии
         for sprite in platforms:
+
             if sprite.rect.colliderect(self.hitbox):
 
                 # Движение вправо
@@ -77,13 +90,14 @@ class Player(pygame.sprite.Sprite):
                 elif self.velocity.x < 0:
                     self.hitbox.left = sprite.rect.right
 
-                # Синхронизация float-позиции
+                # Синхронизация позиции
                 self.pos.x = self.hitbox.x
 
                 # Остановка
                 self.velocity.x = 0
 
     def vertical_movement_collision(self, platforms, dt):
+
         # Гравитация
         self.apply_gravity(dt)
 
@@ -94,26 +108,30 @@ class Player(pygame.sprite.Sprite):
         self.pos.y += self.velocity.y * dt
         self.hitbox.y = round(self.pos.y)
 
-        # Проверка коллизий
+        # Коллизии
         for sprite in platforms:
+
             if sprite.rect.colliderect(self.hitbox):
 
                 # Падение вниз
                 if self.velocity.y > 0:
+
                     self.hitbox.bottom = sprite.rect.top
                     self.on_ground = True
 
                 # Удар головой
                 elif self.velocity.y < 0:
+
                     self.hitbox.top = sprite.rect.bottom
 
-                # Синхронизация float-позиции
+                # Синхронизация позиции
                 self.pos.y = self.hitbox.y
 
-                # Остановка по Y
+                # Остановка
                 self.velocity.y = 0
 
-    def update(self, platforms, dt):
+    def update(self, dt, platforms):
+
         # Ввод
         self.get_input()
 
